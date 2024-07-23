6c2e82ec0b8e24783ab2f7102cd83a7f354c3d8a
nmelgar <nmelgar16013@gmail.com>
2024-07-23 13:59:58 -0600

    Add final requirements, works fine.

--- main.py
+++ main.py
@@ -1,62 +1,33 @@
 # %%
 import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
 
 # %%
 url = "https://github.com/fivethirtyeight/data/blob/master/star-wars-survey/StarWars.csv?raw=true"
 data_sw = pd.read_csv(url, encoding="ISO-8859-1")
 data_sw = data_sw.drop(index=0)
 
-# %%
-data_sw.columns
-
 ## GRAND QUESTION 1
 # Shorten the column names and clean them up for easier use with pandas. Provide a table
 # or list that exemplifies how you fixed the names.
 # %%
-# %%
-# data_sw.columns = [
-#     "RespondentID",
-#     "Seen_any_SW_film",
-#     "SW_fan",
-#     "Seen_EP1",
-#     "Seen_EP2",
-#     "Seen_EP3",
-#     "Seen_EP4",
-#     "Seen_EP5",
-#     "Seen_EP6",
-#     "Rank_EP1",
-#     "Rank_EP2",
-#     "Rank_EP3",
-#     "Rank_EP4",
-#     "Rank_EP5",
-#     "Rank_EP6",
-#     "Han_fav",
-#     "Luke_fav",
-#     "Leia_fav",
-#     "Anakin_fav",
-#     "Obi_fav",
-#     "Emperor_fav",
-#     "Darth_fav",
-#     "Lando_fav",
-#     "Boba_fav",
-#     "C3PO_fav",
-#     "R2D2_fav",
-#     "JarJar_fav",
-#     "Padme_fav",
-#     "Yoda_fav",
-#     "Character_shot_first",
-#     "Expanded_universe_familiarized",
-#     "Expanded_universe_fan",
-#     "Star_Trek_fan",
-#     "Gender",
-#     "Age",
-#     "Income",
-#     "Education",
-#     "Location",
-# ]
-data_sw.columns = [
+# create a df to display the names of the columns olds and news
+new_columns_df = pd.DataFrame(columns=["old_columns", "new_columns"])
+old_cols = data_sw.columns.tolist()
+# insert old column names from data_sw to new 'old_columns' column in new cols df
+new_columns_df["old_columns"] = old_cols
+
+def new_columns(df_frame):
+    # new names for the columns
+    df_frame.columns = [
     "respondent_id",
-    "seen_any_film",
+    "seen_any_sw_film",
     "sw_fan",
     "seen_ep1",
     "seen_ep2",
@@ -94,9 +65,11 @@ data_sw.columns = [
     "education",
     "location",
 ]
+new_columns(data_sw)
+# insert new column names in the new cols df
+new_columns_df["new_columns"] = data_sw.columns.tolist()
 
-# %%
-data_sw.head(15)
+new_columns_df.head(10)
 
 ## GRAND QUESTION 2
 # Clean and format the data so that it can be used in a machine learning model.
@@ -105,11 +78,12 @@ data_sw.head(15)
 # short description of the changes made.
 
 # %%
+# add respondent id an easier to read number
 data_sw["respondent_id"] = range(1, len(data_sw) + 1)
 
 # %%
 #     Filter the dataset to respondents that have seen at least one film
-data_sw = data_sw[data_sw["seen_any_film"] == "Yes"]
+data_sw = data_sw[data_sw["seen_any_sw_film"] == "Yes"]
 
 # %%
 # fill NaN for numerical columns usign the median
@@ -180,7 +154,7 @@ data_sw = data_sw.drop(columns=["income"])
 
 # %%
 #     Create your target (also known as “y” or “label”) column based on the new income range column
-data_sw["target_income"] = data_sw["income_target"].apply(
+data_sw["target_income"] = data_sw["income_group"].apply(
     lambda x: 1 if x > 50000 else 0
 )
 
@@ -188,13 +162,140 @@ data_sw["target_income"] = data_sw["income_target"].apply(
 #     One-hot encode all remaining categorical columns
 data_sw = pd.get_dummies(data_sw, columns=["sw_fan", "gender", "location"])
 
+data_sw.head(10)
 # GRAND QUESTION 3
 # Validate that the data provided on GitHub lines up with the article by recreating
 # 2 of the visuals from the article.
 
+# %%
+# Create chart based on the probability that someone has seen a given “Star Wars”
+# film, given that they have seen any Star Wars film:
+
+data_sw = data_sw[data_sw["seen_any_sw_film"] == "Yes"]
+
+star_wars_movies_list = [
+    "The Phantom Menace",
+    "Attack of the Clones",
+    "Revenge of the Sith",
+    "A New Hope",
+    "The Empire Strikes Back",
+    "Return of the Jedi",
+]
+
+see_movies_list = [
+    "seen_ep1",
+    "seen_ep2",
+    "seen_ep3",
+    "seen_ep4",
+    "seen_ep5",
+    "seen_ep6",
+]
+
+seen_film_counts = pd.DataFrame(columns=["Movie", "Percentage"])
+
+percentages = []
+for seen in see_movies_list:
+    percentage = data_sw[seen].notna().mean() * 100
+    percentages.append(percentage)
+
+seen_film_counts = pd.DataFrame(
+    {"Movie": star_wars_movies_list, "Percentage": percentages}
+)
+
+seen_film_counts["Percentage"] = seen_film_counts["Percentage"].round(0)
+
+# create chart
+seen_fig = px.bar(
+    seen_film_counts,
+    y="Movie",
+    x="Percentage",
+    title="Which 'Star Wars' Movie Have You Seen?",
+    text="Percentage",
+    orientation="h",
+)
+
+seen_fig.show()
+
+# %%
+# create chart do display who shot first? column
+# calculate percentage for each category in shot_first column
+shot_first_counts = data_sw["shot_first"].value_counts(normalize=True) * 100
+shot_first_counts = shot_first_counts.reset_index()
+shot_first_counts.columns = ["shot_first", "percentage"]
+
+# round to whole number
+shot_first_counts["percentage"] = shot_first_counts["percentage"].round(0)
+
+# create chart
+fig = px.bar(
+    shot_first_counts,
+    y="shot_first",
+    x="percentage",
+    title="Who Shot First?",
+    labels={"shot_first": "Character who shot first", "percentage": "Percentage"},
+    text="percentage",
+    orientation="h",
+)
+
+fig.show()
+
+# %%
 
 # # GRAND QUESTION 4
 # Build a machine learning model that predicts whether a person makes more than $50k.
 # Describe your model and report the accuracy.
 
+# %%
+# split the data in features and target label
+X = data_sw.drop(
+    columns=[
+        "target_income",
+        "seen_any_sw_film",
+        "seen_ep1",
+        "seen_ep2",
+        "seen_ep3",
+        "seen_ep4",
+        "seen_ep5",
+        "seen_ep6",
+        "fav_han",
+        "fav_luke",
+        "fav_leia",
+        "fav_anakin",
+        "fav_obi",
+        "fav_emperor",
+        "fav_darth",
+        "fav_lando",
+        "fav_boba",
+        "fav_c3po",
+        "fav_r2d2",
+        "fav_jarjar",
+        "fav_padme",
+        "fav_yoda",
+        "shot_first",
+        "expanded_universe_fam",
+        "expanded_universe_fan",
+        "star_trek_fan",
+    ]
+)
+y = data_sw["target_income"]
+
+# split data in training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42
+)
+
+# standardize features
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+# initialize the random forest classifier
+rf_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_clf.fit(X_train, y_train)
+y_pred = rf_clf.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+
+# show the accuracy
+print(f"The accuracy of the Random Forest Classifier is: {accuracy:.2f}")
+print(classification_report(y_test, y_pred))
 # %%
